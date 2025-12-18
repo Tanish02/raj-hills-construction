1474ec7822739c73a7f248034371864804100831
Tanish Sharma <139219849+Tanish02@users.noreply.github.com>
2025-12-19 02:01:01 +0530

    add company information to data source

---
+++ src/data/Componey.ts
@@ -0,0 +1,11 @@
+export const company = {
+  name: "Raj Hills Construction",
+  phone: "+919560961031",
+  whatsapp: "+919560961031",
+  email: "contact@rajhills.com",
+  address: "Delhi, India",
+};
+
+//end code
+
+// dummy email
