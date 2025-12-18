28e42d7c28096c63e49fe02a0489e8bde3286190
Tanish Sharma <139219849+Tanish02@users.noreply.github.com>
2025-12-19 01:48:12 +0530

    WIP

--- src/components/sections/Projects.tsx
+++ src/components/sections/Projects.tsx
@@ -1,11 +1,49 @@
+import { projects } from "@/data/projects";
+
+import Image from "next/image";
+
 export default function Projects() {
   return (
-    <section id="Projects" className="scroll-mt-24 py-24 bg-black">
-      <h2 className="text-3xl font-semibold mb-4">Projects</h2>
-      <p className="text-gray-600">
-        Raj Hills Construction is a trusted name in residential and commercial
-        development.
-      </p>
+    <section id="projects" className="scroll-mt-24 py-24 bg-black">
+      <div className="max-w-7xl mx-auto px-6">
+        {/* section Header */}
+        <div className="max-w-2xl mb-12">
+          <h2 className="text-3xl md:text-4xl font-semibold text-white">
+            Our Projects
+          </h2>
+          <p className="mt-4 text-gray-400">
+            A selection of residential and commercial projects delivered with
+            quality, precision, and trust.
+          </p>
+        </div>
+
+        {/* Project Grid */}
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+          {projects.map((project) => (
+            <div
+              key={project.id}
+              className="group border border-white/10 rounded-lg overflow-hidden"
+            >
+              {/* Image section*/}
+              <div className="relative h-64">
+                <Image
+                  src={project.image}
+                  alt={project.title}
+                  fill
+                  className="object-cover group-hover:scale-105 transition"
+                />
+              </div>
+              {/* Project Info */}
+              <div className="p-5">
+                <h3 className="text-lg font-medium text-white">
+                  {project.title}
+                </h3>
+                <p className="text-sm text-gray-400 mt-1">{project.location}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </section>
   );
 }
--- src/data/Projects.ts
+++ src/data/Projects.ts
@@ -3,24 +3,24 @@ export const projects = [
     id: "raj-hills-residency",
     title: "Raj Hills Residency",
     location: "Delhi, India",
-    image: "/public/images/project-1.jpg",
+    image: "/images/project-1.jpg",
   },
   {
     id: "raj-hills-villas",
     title: "Raj Hills Villas",
     location: "Delhi, India",
-    image: "/public/images/project-2.jpg",
+    image: "/images/project-2.jpg",
   },
   {
     id: "raj-hills-commercial",
     title: "Raj Hills Commercial",
     location: "Rajasthan, India",
-    image: "/public/images/project-3.jpg",
+    image: "/images/project-3.jpg",
   },
   {
     id: "raj-hills-Homes",
     title: "Raj Hills Homes",
     location: "Jaipur, India",
-    image: "/public/images/project-4.jpg",
+    image: "/images/project-4.jpg",
   },
 ];
